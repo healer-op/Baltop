5c231fc2d1225bf3cedbab4e8057dfa0f576750c
HEALER <65026164+healer-op@users.noreply.github.com>
2021-07-02 20:48:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+//api fetcher by healer op
+fetch('api.json')
+.then(res=> res.json())
+.then(data => {
+    console.log(data.Survival);
+    const html = data.Survival.player_name.map(user =>{
+    return `
+    <div class="gg">
+    <p><img src='"http://cravatar.eu/avatar/"+${user}+"/64.png"' width="64px" alt="logo">${user}</p>
+    </div>`;
+}).join('');
+document.querySelector("#app").insertAdjacentHTML("afterbegin", html);
+}).catch(error =>{
+console.log(error);
+});
+//change the code not credits
